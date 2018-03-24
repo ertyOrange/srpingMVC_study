5ecbe3d4275d6d0cc753d918bfef867e4e245452
ertyOrange <263374542@qq.com>
2018-03-24 18:11:51 +0800

    add spring event 非注解监听器

---
+++ src/springEvent/MyNoAnnotationListener.java
@@ -0,0 +1,15 @@
+package springEvent;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyNoAnnotationListener implements ApplicationListener<MyTestEvent> {
+
+	@Override
+	public void onApplicationEvent(MyTestEvent arg0) {
+		System.out.println("·Ç×¢½â¼àÌýÆ÷£º" + arg0.getMsg());
+
+	}
+
+}
---
+++ src/springEvent/MyTestEventPubLisher.java
@@ -0,0 +1,15 @@
+package springEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTestEventPubLisher {
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	public void pushListener(String msg) {
+		applicationContext.publishEvent(new MyTestEvent(this, msg));
+	}
+}
